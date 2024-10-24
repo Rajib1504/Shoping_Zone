ef8011756b0a9af960507a6023a8b419632b355e
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2024-10-24 14:06:47 +0530

    style done alomost

--- src/Components/Main/Carts/Bookmark/Bookmark.jsx
+++ src/Components/Main/Carts/Bookmark/Bookmark.jsx
@@ -1,5 +1,10 @@
 const Bookmark = () => {
-  return <div></div>;
+  return (
+    <div className="flex justify-between">
+      <p>add on me</p>
+      <p>do</p>
+    </div>
+  );
 };
 
 export default Bookmark;
--- src/Components/Main/Carts/Bookmark/Bookmarks.jsx
+++ src/Components/Main/Carts/Bookmark/Bookmarks.jsx
@@ -1,8 +1,12 @@
+import Bookmark from "./Bookmark";
+
 const Bookmarks = () => {
   return (
     <div className="border-2 border-blue-100 md:col-span-3 sm:col-span-12 rounded-lg">
-      <h1 className="text-2xl text-center">Bookmark Container</h1>
-      <div></div>
+      <h1 className="text-2xl font-bold text-center">Your Cart</h1>
+      <div className="border-2 m-2 shadow-xl p-4">
+        <Bookmark></Bookmark>
+      </div>
     </div>
   );
 };
